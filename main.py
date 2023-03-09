46a48f8acda4731b0d28ae3f63a10849248fc0b3
Stevo <therealstevenb@gmail.com>
2023-03-09 17:29:31 -0600

    imported distance data from csv

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import math
 from package import Package
 from truck import Truck
 
+distanceData = []
+
 
 def input_package_data(file_name):
     with open(file_name) as all_packages:
@@ -18,16 +20,27 @@ def input_package_data(file_name):
             destination_address = package[1]
             city = package[2]
             state = package[3]
-            zip = package[4]
+            zip_code = int(package[4])
             deliver_by = package[5]
-            mass = package[6]
+            mass = int(package[6])
             special_inst = package[7]
 
-            formatted_package = Package(package_id, destination_address, city, state, zip, deliver_by, mass,
+            # create package object using csv values from above
+            formatted_package = Package(package_id, destination_address, city, state, zip_code, deliver_by, mass,
                                         special_inst)
-            # will this allow you to load hash table?
-            # return int(package_id), formatted_package
+            # insert that package object into hash table as key:package_id value: package object
             myHash.insert(package_id, formatted_package)
+
+
+# begin distance data input static function
+def input_distance_data(file_name):
+    with open(file_name) as all_data_dist:
+        distance_data = csv.reader(all_data_dist, delimiter=',')
+        next(distance_data)  # skip header
+        for distances in distance_data:   # need to skip name and address
+            values = distances[2:]
+            distanceData.append(values)
+# end distance data input static function
 
 
 # HashTable class using chaining.
@@ -39,16 +52,6 @@ class ChainingHashTable:
         self.table = []
         for i in range(initial_capacity):
             self.table.append([])
-
-    # ORIGINAL
-    # Inserts a new item into the hash table.
-    # def insert(self, item):
-    #     # use  built-in hash() to define correct bucket and create bucket_list where this item will go.
-    #     bucket = hash(item) % len(self.table)
-    #     bucket_list = self.table[bucket]
-    #
-    #     # insert the item to the end of the bucket list.
-    #     bucket_list.append(item)
 
     def insert(self, key, package):  # does insert and update
         # use  built-in hash() to define correct bucket and create bucket_list where this item will go.
@@ -67,23 +70,6 @@ class ChainingHashTable:
         bucket_list.append(key_value)
         return True
 
-    # ORIGINAL
-    # Searches for an item with matching key in the hash table.
-    # Returns the item if found, or None if not found.
-    # def search(self, key):
-    #     # get the bucket list where this key would be.
-    #     bucket = hash(key) % len(self.table)
-    #     bucket_list = self.table[bucket]
-    #
-    #     # search for the key in the bucket list
-    #     if key in bucket_list:
-    #         # find the item's index and return the item that is in the bucket list.
-    #         item_index = bucket_list.index(key)
-    #         return bucket_list[item_index]
-    #     else:
-    #         # the key is not found.
-    #         return None
-
     def search(self, key):
         # get the bucket list where key is located.
         bucket = hash(key) % len(self.table)
@@ -99,17 +85,6 @@ class ChainingHashTable:
                 return key_value[1]  # value
         return None
 
-    # ORIGINAL
-    # Removes an item with matching key from the hash table.
-    # def remove(self, key):
-    #     # get the bucket list where this item will be removed from.
-    #     bucket = hash(key) % len(self.table)
-    #     bucket_list = self.table[bucket]
-    #
-    #     # remove the item from the bucket list if it is present.
-    #     if key in bucket_list:
-    #         bucket_list.remove(key)
-
     def remove(self, key):
         # get the bucket list where this item will be removed from.
         bucket = hash(key) % len(self.table)
@@ -119,46 +94,27 @@ class ChainingHashTable:
         for kv in bucket_list:
             if kv[0] == key:
                 bucket_list.remove([kv[0], kv[1]])
+                print('Package removed from queue')  # verify remove method
 
-
-# bestMovies = [
-#     [1, 'Citizen Kane -1941'],
-#     [2, 'Casablanca - 1942'],
-#     [3, 'The Godfather - 1972'],
-#     [4, 'Gone with the Wind - 1939'],
-#     [5, 'Lawrence of Arabia - 1962'],
-#     [6, 'The Wizard of Oz - 1939'],
-#     [7, 'The Graduate - 1967'],
-#     [8, 'On the Waterfront - 1954'],
-#     [9, 'Schindler\'s List - 1993'],
-#     [10, 'Singin\' in the Rain - 1952'],
-#     [11, 'Star Wars - 1977']
-# ]
 
 myHash = ChainingHashTable(50)
-# myHash.insert(bestMovies[0][0], bestMovies[0][1])
-# print(myHash.table)
-#
-# myHash.insert(bestMovies[10][0], bestMovies[10][1])
-# print(myHash.table)
-#
-# myHash.insert(1, 'Star Trek - 1979')
-# print(myHash.table)
-#
-# print(myHash.search(1))
-# print(myHash.search(11))
-#
-# myHash.remove(1)
-# print(myHash.search(1))
-# print(myHash.table)
-#
-# myHash.remove(11)
-# print(myHash.search(11))
-# print(myHash.table)
 
 # Load package data from CSV
 input_package_data('WGUPS_Package_File.csv')
 
 # test print hash table
-for i in range(len(myHash.table)):
-    print('ID: {}; Package Info: {}'.format(i+1, myHash.search(i+1)))
+# for i in range(len(myHash.table)):
+#     print('ID: {}; Package Info: {}'.format(i+1, myHash.search(i+1)))
+
+# test insert/remove to hash table
+# test_package = Package(50, '123 Penny Lane', 'Hollywood', 'CA', 90210, 'EOD', 15, 'signed delivery')
+# myHash.insert(50, test_package)
+# print(myHash.search(50))
+#
+# myHash.remove(50)
+# print(myHash.search(50))
+
+# Load distance data from CSV
+input_distance_data('WGUPS_Distance_Table.csv')
+
+print(distanceData)
--- package.py
+++ package.py
@@ -15,22 +15,3 @@ class Package:
 
     def __str__(self):  # overwrite print() to print as string, not reference
         return f'{self.package_id}, {self.destination_address}, {self.city}, {self.state}, {self.zip}, {self.deliver_by}, {self.mass}, {self.special_inst}'
-
-    # ---------------   Does this go here or in main???   --------------------------
-    # def input_package_data(file_name):
-    #     with open(file_name) as all_packages:
-    #         package_data = csv.reader(all_packages, delimiter=',')
-    #         next(package_data)  # SKIP header or delete this line and header line in CSV later
-    #         for package in package_data:
-    #             package_id = int(package[0])
-    #             destination_address = package[1]
-    #             city = package[2]
-    #             state = package[3]
-    #             deliver_by = package[4]
-    #             mass = package[5]
-    #             special_inst = package[6]
-    #
-    #             formatted_package = Package(package_id, destination_address, city, state, deliver_by, mass,
-    #                                         special_inst)
-    #             # will this allow you to load hash table?
-    #             # return int(package_id), formatted_package

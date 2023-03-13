1e5ec115ca75fe41b0c9da0c03c7124d9b334416
Stevo <therealstevenb@gmail.com>
2023-03-13 10:30:41 -0500

    chaininghashtable.py created cleaned up main.py by moving ChainingHashTable class to separate *.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 
 import csv
 import math
+from chaininghashtable import ChainingHashTable
 from package import Package
 from truck import Truck
 
@@ -196,60 +197,6 @@ def deliver_packages(truck):        # ----------- UNDER CONSTRUCTION -----------
         if distance_between(current_loc, package_item.destination_address) < shortest:
             shortest = distance_between(current_loc, package_item.destination_address)
             next_stop = package_item.destination_address
-
-
-# HashTable class using chaining.
-class ChainingHashTable:
-    # Constructor with optional initial capacity parameter.
-    # Assigns all buckets with an empty list.
-    def __init__(self, initial_capacity=10):
-        # initialize hash table with empty bucket list entries.
-        self.table = []
-        for i in range(initial_capacity):
-            self.table.append([])
-
-    def insert(self, key, package):  # does insert and update
-        # use  built-in hash() to define correct bucket and create bucket_list where this item will go.
-        bucket = hash(key) % len(self.table)
-        bucket_list = self.table[bucket]
-
-        # update key if already in bucket
-        for kv in bucket_list:
-            # print(kv)
-            if kv[0] == key:
-                kv[1] = package
-                return True
-
-        # if not, insert the item to the end of the bucket list.
-        key_value = [key, package]
-        bucket_list.append(key_value)
-        return True
-
-    def search(self, key):
-        # get the bucket list where key is located.
-        bucket = hash(key) % len(self.table)
-        bucket_list = self.table[bucket]
-        # print(bucket)
-        # print(bucket_list)
-
-        # search for the key in the bucket list
-        for key_value in bucket_list:
-            # print(key_value)
-            # find the item's index and return the item that is in the bucket list.
-            if key_value[0] == key:
-                return key_value[1]  # value
-        return None
-
-    def remove(self, key):
-        # get the bucket list where this item will be removed from.
-        bucket = hash(key) % len(self.table)
-        bucket_list = self.table[bucket]
-
-        # remove the item from the bucket list if it is present.
-        for kv in bucket_list:
-            if kv[0] == key:
-                bucket_list.remove([kv[0], kv[1]])
-                # print('Package removed from queue')  # verify remove method
 
 
 myHash = ChainingHashTable(50)
